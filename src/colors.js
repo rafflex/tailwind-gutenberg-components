191a354bc11befedb626c3c3b31f1eb303b8c1d9
Daniel Sturm <d.sturm@zirkeldesign.de>
2020-07-06 11:21:08 +0200

    Change fallback to backgroundOpacity

--- src/colors.js
+++ src/colors.js
@@ -30,7 +30,7 @@ module.exports = pluginWithDefaultConfig(({ addComponents, theme }) => {
       `${color}`.trim().match(/^(#|rgb|hsl|hsv|hsi|lab|lch|hcl|lrgb)/),
   );
 
-  const opacities = theme('gutenberg.opacity', theme('opacity'));
+  const opacities = theme('gutenberg.opacity', theme('backgroundOpacity'));
 
   const shades = _.map(validColors, (color, name) =>
     _.map(opacities, (opacity) => ({
